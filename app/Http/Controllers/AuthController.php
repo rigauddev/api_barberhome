bf546ea443b622e1fe16853508d952998d5ee9a2
Luis Rigaud <luisrigaud@Luiss-MacBook-Air.local>
2023-04-11 18:02:01 -0300

    Create api barbaer_home

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AuthController extends Controller
+{
+    public function name() {
+        return 'name';
+    }
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+//        dd(Auth::guard('api')->attempt($credentials));
+        if (! $token = Auth::guard('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $user = User::where('name', $request['name'])->first();
+
+        if (! $user){
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->activated = 1;
+            $user->save();
+
+            return response()->json(['message' => 'User created successful.', 'user' => $user]);
+
+        }else{
+
+            return response()->json(['message' => 'ERRO: User not created'], 400);
+
+        }
+    }
+
+    public function me()
+    {
+        return response()->json(auth('api')->user());
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'user' => auth('api')->user(),
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy buildin g your API!
+|
+*/
+
+Route::group(['middleware' => 'api'], function ($router) {
+
+    Route::get('/', fn() => response()->json(['messege' => 'api barber home', 'status' => 'conected']));
+
+    Route::fallback(function (){
+        return response()->json(['message'=> 'Route not found', 'status' => 'connected']);
+    });
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
+
